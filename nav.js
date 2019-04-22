377e4a57e825b26b0914c695413f96e778dae29e
jeff3071 <jeff563715@gmail.com>
2019-04-22 16:59:14 +0800

    init

---
+++ nav.js
@@ -0,0 +1,11 @@
+header = '<ul id="navbar">\
+        <li><a href="index.html">課程整合平台</a></li>\
+        <li><a href="index.html">查詢教授</a></li>\
+        <li><a href = "https://hackmd.io/c/H15623854/%2Fj1roeJwERveYFccP0XZvPg">最新活動</a></li >\
+        <li><a href="exp.html">經驗分享</a></li>\
+        <li><a href="http://140.123.4.10/eportfolio/exam/exam.php">生涯性向測驗</a></li>\
+        <li><a href="#">客製化學程</a></li>\
+        <li><a href="#">關於我們</a></li>\
+    </ul>';
+
+document.write(header);
