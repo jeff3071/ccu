f637f5dd1bd5c3e8376e1e286bdbb5fbd34c3713
jeff3071 <jeff563715@gmail.com>
2019-05-03 00:55:24 +0800

    客製化學程頁籤實作

--- nav.js
+++ nav.js
@@ -4,7 +4,7 @@ header = '<ul id="navbar">\
         <li><a href = "https://hackmd.io/c/H15623854/%2Fj1roeJwERveYFccP0XZvPg">最新活動</a></li >\
         <li><a href="exp.html">經驗分享</a></li>\
         <li><a href="http://140.123.4.10/eportfolio/exam/exam.php">生涯性向測驗</a></li>\
-        <li><a href="#">客製化學程</a></li>\
+        <li><a href="intro.html">客製化學程</a></li>\
         <li><a href="#">關於我們</a></li>\
     </ul>';
 
